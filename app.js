2bd2c9eff070265a29119d244a014c130e133875
subjitdas <subjitdas@gmail.com>
2020-08-14 20:42:38 +0530

    Added server side signup validation

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose');
 const multer = require('multer');
 
 const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
 
 const MONGODB_URI = 'mongodb+srv://'+process.env.user+':'+process.env.password+'@cluster0-fzxis.mongodb.net/social-media';
 
@@ -45,13 +46,16 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
 
 app.use((error, req, res, next) => {
     console.log(error);
     const status = error.statusCode || 500;
     const message = error.message;
+    const data = error.data;
     res.status(status).json({
-        message: message
+        message: message,
+        data: data
     })
 });
 
